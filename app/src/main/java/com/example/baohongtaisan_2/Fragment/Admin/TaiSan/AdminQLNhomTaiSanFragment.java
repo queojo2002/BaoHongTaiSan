e9ac9ee2d339985a0608d8c187beda9003ad7a04
PhucHieu <hieubien811703@gmail.com>
2023-10-24 11:37:02 +0700

    Xong "nhóm tài sản"

--- app/src/main/java/com/example/baohongtaisan_2/Fragment/Admin/TaiSan/AdminQLNhomTaiSanFragment.java
+++ app/src/main/java/com/example/baohongtaisan_2/Fragment/Admin/TaiSan/AdminQLNhomTaiSanFragment.java
@@ -1,65 +1,137 @@
 package com.example.baohongtaisan_2.Fragment.Admin.TaiSan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.baohongtaisan_2.Activity.Admin.TaiSan.AdminNhomTaiSanAddActivity;
+import com.example.baohongtaisan_2.Adapter.Admin.AdminNhomTaiSanAdapter;
+import com.example.baohongtaisan_2.Api.ApiServices;
+import com.example.baohongtaisan_2.Model.NhomTaiSan;
 import com.example.baohongtaisan_2.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminQLNhomTaiSanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 public class AdminQLNhomTaiSanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private SearchView SVnts;
+    private RecyclerView rcvNTS;
+    private AdminNhomTaiSanAdapter nts_adapter;
+    private List<NhomTaiSan> listNTS;
+    private Button btnaddnts;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public AdminQLNhomTaiSanFragment() {
-        // Required empty public constructor
+    private void initializeViews(View rootView) {
+        rcvNTS = rootView.findViewById(R.id.rvNhomTS);
+
+        SVnts = rootView.findViewById(R.id.txtSearchNhomTS);
+
+        btnaddnts = rootView.findViewById(R.id.btnnhomts_add);
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminQLNhomTaiSanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminQLNhomTaiSanFragment newInstance(String param1, String param2) {
-        AdminQLNhomTaiSanFragment fragment = new AdminQLNhomTaiSanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private void setupButtonClickListeners() {
+
+        btnaddnts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), AdminNhomTaiSanAddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void setupRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext());
+        rcvNTS.setLayoutManager(linearLayoutManager);
+
+        listNTS = new ArrayList<>();
+        nts_adapter = new AdminNhomTaiSanAdapter(listNTS);
+        rcvNTS.setAdapter(nts_adapter);
+
+        GetListNTS();
+    }
+
+    private void setupSearchViewListeners() {
+        SVnts.clearFocus();
+        SVnts.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchNTS(s);
+                return false;
+            }
+        });
+
+    }
+
+    public void GetListNTS() {
+        ApiServices.apiServices.get_list_nhomtaisan().enqueue(new Callback<List<NhomTaiSan>>() {
+            @Override
+            public void onResponse(Call<List<NhomTaiSan>> call, Response<List<NhomTaiSan>> response) {
+                listNTS.clear();
+                listNTS = response.body();
+                if (listNTS != null && getContext() != null) {
+                    nts_adapter = new AdminNhomTaiSanAdapter(listNTS);
+                    rcvNTS.setAdapter(nts_adapter);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<NhomTaiSan>> call, Throwable t) {
+                Toast.makeText(getContext(), "Lấy dữ liệu thất bại...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void searchNTS(String text) {
+        ArrayList<NhomTaiSan> searchlist = new ArrayList<>();
+        for (NhomTaiSan nhomTaiSan : listNTS) {
+            if (nhomTaiSan.getTenNTS().toLowerCase().contains(text.toLowerCase())) {
+                searchlist.add(nhomTaiSan);
+            }
+        }
+        nts_adapter.searchDataList(searchlist);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onResume() {
+        super.onResume();
+        GetListNTS();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_q_l_nhom_tai_san, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_admin_q_l_nhom_tai_san, container, false);
+        initializeViews(rootView);
+
+        setupRecyclerView();
+        setupSearchViewListeners();
+        setupButtonClickListeners();
+
+        return rootView;
     }
 }
