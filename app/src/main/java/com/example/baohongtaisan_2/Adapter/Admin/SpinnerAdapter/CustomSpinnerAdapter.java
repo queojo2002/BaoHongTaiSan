b674bcc0d28d74309520a39028144e6912e7c39f
PhucHieu <hieubien811703@gmail.com>
2023-10-26 12:14:45 +0700

    12.14pm 261023

---
+++ app/src/main/java/com/example/baohongtaisan_2/Adapter/Admin/SpinnerAdapter/CustomSpinnerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.baohongtaisan_2.Adapter.Admin.SpinnerAdapter;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomSpinnerAdapter extends ArrayAdapter<CharSequence> {
+    private Context context;
+    private int resource;
+    private int textViewResourceId;
+    private List<CharSequence> items;
+    private int textSize;
+
+    public CustomSpinnerAdapter(Context context, int resource, int textViewResourceId, List<CharSequence> items, int textSize) {
+        super(context, resource, textViewResourceId, items);
+        this.context = context;
+        this.resource = resource;
+        this.textViewResourceId = textViewResourceId;
+        this.items = items;
+        this.textSize = textSize;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View view = super.getDropDownView(position, convertView, parent);
+        TextView textView = view.findViewById(textViewResourceId);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+        return view;
+    }
+}
--- app/src/main/java/com/example/baohongtaisan_2/Fragment/Admin/AdminBaoHongFragment.java
+++ app/src/main/java/com/example/baohongtaisan_2/Fragment/Admin/AdminBaoHongFragment.java
@@ -1,25 +1,33 @@
 package com.example.baohongtaisan_2.Fragment.Admin;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.baohongtaisan_2.Adapter.Admin.AdminBaoHongAdapter;
+import com.example.baohongtaisan_2.Adapter.Admin.SpinnerAdapter.CustomSpinnerAdapter;
 import com.example.baohongtaisan_2.Api.ApiServices;
 import com.example.baohongtaisan_2.Model.BaoHong;
 import com.example.baohongtaisan_2.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -33,6 +41,7 @@ public class AdminBaoHongFragment extends Fragment {
     private LinearLayoutManager linearLayoutManager;
     private AdminBaoHongAdapter adminBaoHongAdapter;
     private List<BaoHong> baoHongList;
+    private List<BaoHong> originalBaoHongList;
     private View view;
 
     @Override
@@ -56,6 +65,48 @@ public class AdminBaoHongFragment extends Fragment {
         adminBaoHongAdapter = new AdminBaoHongAdapter(baoHongList);
         rvBH.setAdapter(adminBaoHongAdapter);
 
+        Spinner spinner1 = view.findViewById(R.id.spntinhtrang);
+        ArrayAdapter<CharSequence> adapter1 = new CustomSpinnerAdapter(getContext(), android.R.layout.simple_spinner_item, android.R.id.text1, Arrays.asList(getResources().getStringArray(R.array.tinh_trang_array)), 10);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = view.findViewById(R.id.spntrangthai);
+        ArrayAdapter<CharSequence> adapter2 = new CustomSpinnerAdapter(getContext(), android.R.layout.simple_spinner_item, android.R.id.text1, Arrays.asList(getResources().getStringArray(R.array.trang_thai_array)), 10);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedTinhTrang = spinner1.getSelectedItem().toString();
+                int tinhTrangToSearch = getTinhTrangValue(selectedTinhTrang);
+                String selectedTrangThai = spinner2.getSelectedItem().toString();
+                int trangThaiToSearch = getTrangThaiValue(selectedTrangThai);
+                ((TextView) parentView.getChildAt(0)).setTextSize(10);
+                filterData(tinhTrangToSearch, trangThaiToSearch);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedTinhTrang = spinner1.getSelectedItem().toString();
+                int tinhTrangToSearch = getTinhTrangValue(selectedTinhTrang);
+                String selectedTrangThai = spinner2.getSelectedItem().toString();
+                int trangThaiToSearch = getTrangThaiValue(selectedTrangThai);
+                ((TextView) parentView.getChildAt(0)).setTextSize(10);
+
+                filterData(tinhTrangToSearch, trangThaiToSearch);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
 
         svBH.clearFocus();
         svBH.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -99,11 +150,16 @@ public class AdminBaoHongFragment extends Fragment {
         ApiServices.apiServices.get_list_baohong().enqueue(new Callback<List<BaoHong>>() {
             @Override
             public void onResponse(Call<List<BaoHong>> call, Response<List<BaoHong>> response) {
-                baoHongList.clear();
-                baoHongList = response.body();
-                if (baoHongList != null && getContext() != null) {
-                    adminBaoHongAdapter = new AdminBaoHongAdapter(baoHongList);
-                    rvBH.setAdapter(adminBaoHongAdapter);
+                if (response.isSuccessful() && response.body() != null) {
+                    originalBaoHongList = response.body();
+                    baoHongList.clear();
+                    baoHongList.addAll(originalBaoHongList);
+                    if (baoHongList != null && getContext() != null) {
+                        adminBaoHongAdapter = new AdminBaoHongAdapter(baoHongList);
+                        rvBH.setAdapter(adminBaoHongAdapter);
+                    }
+                } else {
+                    Toast.makeText(getContext(), "Có lỗi xảy ra...", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -123,5 +179,55 @@ public class AdminBaoHongFragment extends Fragment {
     }
     public interface ItemClickListener {
         void onClick(View view, int position,boolean isLongClick);
+    }
+    private int getTinhTrangValue(String tinhTrang) {
+        if (tinhTrang.equals("Hư hỏng nhẹ (Minor)")) {
+            return 1;
+        } else if (tinhTrang.equals("Hư hỏng trung bình (Moderate)")) {
+            return 2;
+        } else if (tinhTrang.equals("Hư hỏng nghiêm trọng (Severe)")) {
+            return 3;
+        } else if (tinhTrang.equals("Hư hỏng hoàn toàn (Critical)")) {
+            return 4;
+        } else if (tinhTrang.equals("--Lọc theo tình trạng--")) {
+            return 0;
+        }
+        return -1;
+    }
+
+
+    private int getTrangThaiValue(String tinhTrang) {
+        if (tinhTrang.equals("Đã gửi báo hỏng")) {
+            return 1;
+        } else if (tinhTrang.equals("Đã tiếp nhận báo hỏng")) {
+            return 2;
+        } else if (tinhTrang.equals("Đang sửa chữa")) {
+            return 3;
+        } else if (tinhTrang.equals("Sửa thành công")) {
+            return 4;
+        } else if (tinhTrang.equals("Sửa không thành công")) {
+            return 5;
+        } else if (tinhTrang.equals("--Lọc theo trạng thái--")) {
+            return 0;
+        }
+        return -1;
+    }
+    private void filterData(int tinhTrangToSearch, int trangThaiToSearch) {
+        if (tinhTrangToSearch == 0 && trangThaiToSearch == 0) {
+            // Khi chọn cả "--Lọc theo tình trạng--" và "--Lọc theo trạng thái--"
+            baoHongList.clear();
+            baoHongList.addAll(originalBaoHongList);
+        } else {
+            if (originalBaoHongList != null) {
+                baoHongList.clear();
+                for (BaoHong baoHong : originalBaoHongList) {
+                    if ((tinhTrangToSearch == 0 || baoHong.getTinhTrang() == tinhTrangToSearch) &&
+                            (trangThaiToSearch == 0 || baoHong.getTrangThai() == trangThaiToSearch)) {
+                        baoHongList.add(baoHong);
+                    }
+                }
+            }
+        }
+        adminBaoHongAdapter.notifyDataSetChanged();
     }
 }
